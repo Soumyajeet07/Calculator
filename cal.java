0ed765952c585e0132c285c79e0b8b0faeef7b18
Soumyajeet07 <soumyajeetsinha97@gmail.com>
2019-12-26 20:22:56 -0800

    first change

---
+++ cal.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class calculator{
+public static void main(String[] args){
+int a,b,add;
+Scanner s= new Scanner(System.in);
+a=s.nextInt();
+b=s.nextInt();
+add= a + b;
+system.out.println("The result is:" +add);
+}
+}		
